9463e85993a640a4881b3417997cee6a1b23749a
Aringoaway <a.konshina.job@gmail.com>
2022-03-13 12:02:27 +0100

    add date in Card component

--- src/App.js
+++ src/App.js
@@ -2,28 +2,59 @@ import Card from './components/Card'
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
-function App() {
-  return (
-    <div className="wrapper clear">
-        <Drawer/>
-        <Header/>
+const arr = [
+    {
+        title: 'Nike Blazer Mid Suede Men\'s Sneakers',
+        price: 12999,
+        imageUrl: '/img/sneakers/1.jpg'
+    },
+    {
+        title: 'Nike Air Max 270 Men\'s Running Shoes',
+        price: 15609,
+        imageUrl: '/img/sneakers/2.jpg'
+    },
+    {
+        title: 'Nike Blazer Mid Suede Men\'s Sneakers',
+        price: 8499,
+        imageUrl: '/img/sneakers/3.jpg'
+    },
+    {
+        title: 'Puma X Aka Boku Future Rider sneakers',
+        price: 8999,
+        imageUrl: '/img/sneakers/4.jpg'
+    }
+];
 
-        <div className="content p-40">
-            <div className="d-flex align-center justify-between mb-40">
-                <h1>All sneakers</h1>
-                <div className="search-block d-flex">
-                    <img src="/img/search.svg" alt="Search"/>
-                    <input placeholder="Search..." type="text"/>
+function App() {
+    return (
+        <div className="wrapper clear">
+            <Drawer/>
+            <Header/>
+
+            <div className="content p-40">
+                <div className="d-flex align-center justify-between mb-40">
+                    <h1>All sneakers</h1>
+                    <div className="search-block d-flex">
+                        <img src="/img/search.svg" alt="Search"/>
+                        <input placeholder="Search..." type="text"/>
+                    </div>
+                </div>
+                <div className="d-flex">
+
+                    {
+                        arr.map((obj) => (
+                            <Card
+                                title={obj.title}
+                                price={obj.price}
+                                imgUrl={obj.imageUrl}
+                            />
+                        ))
+                    }
+
                 </div>
             </div>
-            <div className="d-flex">
-            {/*first card*/}
-                <Card/>
-
-            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
-function Card() {
+function Card(props) {
     return(
         <div className="card">
             <div className="favorite">
                 <img src="/img/heart-unliked.svg" alt="Unliked"/>
             </div>
 
-            <img width={133} height={112} src="/img/sneakers/1.jpg" alt=""/>
-            <p>Nike Blazer Mid Suede Men's Sneakers</p>
+            <img width={133} height={112} src={props.imgUrl} alt=""/>
+            <p>{props.title}</p>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column ">
                     <span>Price:</span>
-                    <b>12 999 Kč</b>
+                    <b>{props.price}</b>
                 </div>
                 <button className="button">
                     <img width={11} height={11} src="/img/plus.svg" alt=""/>
