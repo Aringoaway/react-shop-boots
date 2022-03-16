fc50f2f39ca3c9611b3dd6ed136843b2b120173b
Aringoaway <a.konshina.job@gmail.com>
2022-03-16 21:42:40 +0100

    add useState to Header, Drawer, Cart

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,15 @@
+import React from "react";
 import styles from './Card.module.scss';
-console.log(styles)
+
 
 function Card(props) {
-    const onClickBtn =  () => {
-        alert(props.title)
+    const [isAdded, setIsAdded] = React.useState(false);
+    const onClickPlus = () => {
+        setIsAdded(!isAdded);
     }
-
     return(
         <div className={styles.card}>
-            <div className={styles.favorite}>
+            <div className={styles.favorite} onClick={props.onFavorit}>
                 <img src="/img/heart-unliked.svg" alt="Unliked"/>
             </div>
 
@@ -19,9 +20,12 @@ function Card(props) {
                     <span>Price:</span>
                     <b>{props.price}</b>
                 </div>
-                <button className="button" onClick={onClickBtn}>
-                    <img width={11} height={11} src="/img/plus.svg" alt=""/>
-                </button>
+                    <img
+                        className={styles.plus}
+                        onClick={onClickPlus}
+                        src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"}
+                        alt=""
+                    />
             </div>
         </div>
     );
