37291078cd68922f0da9a56a3c8fb9b218fc5629
Aringoaway <a.konshina.job@gmail.com>
2022-03-20 12:37:50 +0100

    add async to onAddToFavorite

--- src/App.js
+++ src/App.js
@@ -39,12 +39,12 @@ function App() {
         setCartItems((prev) => prev.filter(item => item.id !== id ));
     }
 
-    const onAddToFavorite = (obj) => {
+    const onAddToFavorite = async (obj) => {
         console.log(obj);
         if (favorites.find((favObj) => favObj.id == obj.id)) {
             axios.delete(`https://623475ebdebd056201e599c9.mockapi.io/Favorites/${obj.id}`);
         } else {
-            axios.post('https://623475ebdebd056201e599c9.mockapi.io/Favorites', obj);
+            const resp = await axios.post('https://623475ebdebd056201e599c9.mockapi.io/Favorites', obj);
             setFavorites((prev) => [...prev, obj]);
         }
 
